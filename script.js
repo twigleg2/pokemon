951941a04eca4faeb59bc206c2b0eefd039b4758
Gaven Finch <gavenfinch@gmail.com>
2019-10-24 23:09:08 -0600

    Add difficulty function

--- script.js
+++ script.js
@@ -1,26 +1,94 @@
 var app = new Vue({
     el: '#app',
     data: {
-        extension: '',
+        sprite: '',
+        choices: [],
+        message: '',
+        ready: false,
+        pokedexSize: 0,
+        difficulties: ['easy', 'hard'],
+        difficulty: 0,
+    },
+    created: function () {
+        //get the national pokedex size
+        this.GET("pokedex/1", this.setPokedexSize);
+    },
+    computed: {
+        getDifficulty() {
+            return this.difficulties[this.difficulty];
+        },
+        changeDifficulty() {
+
+            this.difficulty = this.difficulty++ % this.difficulties.length;
+        }
     },
     methods: {
-        fetchREST() {
-            var url = "https://pokeapi.co/api/v2/" + this.extension;
+        async GET(apiParam, exec) {
+            var url = "https://pokeapi.co/api/v2/" + apiParam;
             console.log("URL: " + url);
-            fetch(url)
-                .then((data) => {
-                    return (data.json());
-                })
-                .then((json) => {
-                    console.log("JSON:");
-                    console.log(json);
-                    this.cities = [];
-                    for (let i = 0; i < citylist.length; i++) {
-                        console.log(citylist[i].city);
-                        this.cities.push({ name: citylist[i].city });
-                    };
-                    console.log("Got Citylist");
-                });
+            try {
+                let response = await axios.get(url);
+                let json = response.data;
+                console.log(json);
+                if (exec) {
+                    console.log("executing");
+                    exec(json);
+                }
+                return new Promise((resolve, reject) => resolve());
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+        async getRound() {
+            let apiParam = "pokemon/";
+            this.ready = false;
+            this.message = '';
+            this.sprite = '';
+            this.choices = [];
+
+
+            await Promise.all([
+                this.GET(apiParam + this.getRandomPokedexNumber() + '/', this.getRightAnswer), // one right answer
+                this.GET(apiParam + this.getRandomPokedexNumber() + '/', this.getWrongAnswer), // three wrong answers
+                this.GET(apiParam + this.getRandomPokedexNumber() + '/', this.getWrongAnswer), // TODO: currently possible to get duplicates in the choices array
+                this.GET(apiParam + this.getRandomPokedexNumber() + '/', this.getWrongAnswer),
+            ]);
+            this.shuffle();
+            this.ready = true;
+
+        },
+        checkAnswer(answer) {
+            if (answer.correct) {
+                this.message = "CORRECT!";
+            }
+            else {
+                correctAnswer = this.choices.find(item => { return item.correct })
+                this.message = "INCORRECT! The correct answer is " + correctAnswer.name;
+            }
+        },
+        shuffle() {
+            for (let i = this.choices.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * i)
+                const temp = this.choices[i]
+                this.choices[i] = this.choices[j]
+                this.choices[j] = temp
+            }
+            console.log(this.choices);
+        },
+        getRandomPokedexNumber() {
+            return Math.floor(Math.random() * this.pokedexSize);
+        },
+        getRightAnswer(json) {
+            this.sprite = json.sprites.front_default;
+            this.choices.push({ name: json.name, correct: true });
+        },
+        getWrongAnswer(json) {
+            this.choices.push({ name: json.name, correct: false });
+        },
+        setPokedexSize(json) {
+            this.pokedexSize = json.pokemon_entries.length;
+            console.log(this.pokedexSize);
         },
     },
 });
